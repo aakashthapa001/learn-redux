89f767b4fe05cbe20300d131366d4426bdcde36b
Aakash Thapa <aakash.thapa@agileitsolutions.net>
2016-12-12 15:26:14 +0545

    Init commit

---
+++ app/redux-example.jsx
@@ -0,0 +1,41 @@
+var redux = require('redux');
+
+console.log('Starting redux example');
+
+// Pure function
+// function add(a, b) {
+//   return a + b;
+// }
+//
+// // Not Pure function
+// var a = 3;
+// function add(b) {
+//   return a + b;
+// }
+//
+// var result;
+// function add(a, b) {
+//   result = a + b;
+//   return result;
+// }
+//
+// function add(a, b) {
+//   return a + b + new Date().getSeconds();
+// }
+//
+//
+//
+// function changeProp(obj) {
+//   return {
+//     ...obj,
+//     name: 'Ajit'
+//   };
+// }
+//
+// var startingValue = {
+//   name: 'Aakash',
+//   age: 22
+// };
+// var res = changeProp(startingValue);
+// console.log(startingValue);
+// console.log(res);
