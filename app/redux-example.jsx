253536d8536abedc53200857290d8c6593bb3b54
Aakash Thapa <aakash.thapa@agileitsolutions.net>
2016-12-14 14:53:35 +0545

    Add reducer and start redux

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -4,11 +4,11 @@ console.log('Starting redux example');
 
 var reducer = (state = {name: 'Anonymous'}, action) => {
   // state = state || {name: 'Anonymous'};
-
   return state;
 };
 var store = redux.createStore(reducer);
 var currentState = store.getState();
+console.log('currentState', currentState);
 
 // ===================================================== //
 // Pure function
