7eddfe1fad9dd4667633ebaf8f6f44f32c5af6fa
chimseryOo001 <chimsemgr001@gmail.com>
2016-12-14 20:24:52 +0545

    Add support for location fetching

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,8 +1,9 @@
-var redux = require('redux');
+var redux = require('redux'),
+    axios = require('axios');
 
 console.log('Starting redux example');
 
-// Name reducer and action generator
+// Name reducer and action generators
 // ---------------------------------
 var nameReducer = (state = 'Anonymous', action) => {
   switch(action.type) {
@@ -21,7 +22,7 @@ var changeName = (name) => {
   };
 };
 
-// Hobbies reducer and action generator
+// Hobbies reducer and action generators
 // ---------------------------------
 var nextHobbyId = 1;
 var hobbiesReducer = (state = [], action) => {
@@ -57,7 +58,7 @@ var removeHobby = (id) => {
   };
 };
 
-// Movies reducer and action generator
+// Movies reducer and action generators
 // ---------------------------------
 var nextMoviesId = 1;
 var moviesReducer = (state = [], action) => {
@@ -95,15 +96,63 @@ var removeMovie = (id) => {
   };
 };
 
+// Map reducer and action generators
+// ---------------------------------
+var mapReducer = (state = {isFetching: false, url: undefined}, action) => {
+  switch(action.type) {
+    case 'START_LOCATION_FETCH':
+      return {
+        isFetching: true,
+        url: undefined
+      };
+    case 'COMPLETE_LOCATION_FETCH':
+      return {
+        isFetching: false,
+        url: action.url
+      };
+    default:
+      return state;
+  }
+};
+
+// Start location fetch action
+var startLocationFetch = () => {
+  return {
+    type: 'START_LOCATION_FETCH'
+  };
+};
+
+// Complete location fetch action
+var completeLocationFetch = (url) => {
+  return {
+    type: 'COMPLETE_LOCATION_FETCH',
+    url
+  };
+};
+
+// Fetch location action from API
+var fetchLocation = () => {
+  store.dispatch(startLocationFetch());
+
+  axios.get('http://ipinfo.io').then(function(res) {
+    var loc = res.data.loc;
+    var baseUrl = 'http://maps.google.com?q=';
+
+    store.dispatch(completeLocationFetch(baseUrl + loc));
+  });
+};
+
 // final combined reducer from other reducers
 // ---------------------------------
 var reducer = redux.combineReducers({
   name: nameReducer,
   hobbies: hobbiesReducer,
-  movies: moviesReducer
+  movies: moviesReducer,
+  map: mapReducer
 });
 
-
+// Create store
+// ---------------------------------
 var store = redux.createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 // Subscribe to changes
@@ -112,10 +161,18 @@ var unsubcribe = store.subscribe(() => {
   var state = store.getState();
 
   console.log('Name is', state.name);
-  document.getElementById('app').innerHTML = state.name;
-
   console.log('New state', store.getState());
+
+  if(state.map.isFetching) {
+    document.getElementById('app').innerHTML = 'Loading...';
+  } else if (state.map.url) {
+    document.getElementById('app').innerHTML = '<a href="'+ state.map.url +'" target="_blank">View Your Location</a>'
+  }
 });
+// unsubcribe();
+
+// Fetch location dispatch
+fetchLocation();
 
 var currentState = store.getState();
 console.log('currentState', currentState);
